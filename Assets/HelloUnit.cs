5796ce11489631d97d75b5fc5c107cf094b3f9a8
영우 <ieeh0122@naver.com>
2022-02-09 04:13:13 +0900

    Practice

--- Assets/HelloUnit.cs
+++ Assets/HelloUnit.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HelloUnit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello Unity");
     }
 
     // Update is called once per frame
