54f841e729ac089a58cbdc9d9d141e4985523127
영우 <ieeh0122@naver.com>
2022-02-12 02:18:19 +0900

    Unity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,83 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 1. 위치 벡터
-// 2. 방향 벡터
-struct MyVector
+
+class Tank
 {
-    public float x;
-    public float y;
-    public float z;
-
-    public float magnitude { get { return Mathf.Sqrt(x*x + y*y + z*z); } }
-    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
-    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
-
-    public static MyVector operator +(MyVector a,MyVector b)
-    {
-        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
-    }
-
-    public static MyVector operator -(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
-    }
-    public static MyVector operator *(MyVector a, float d)
-    {
-        return new MyVector(a.x * d, a.y * d, a.z * d);
-    }
+    public float speed = 10.0f;
 }
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    float _speed = 10.0f;
+    float _speed = 15.0f;
 
     void Start()
     {
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
 
+        Tank tank1 = new Tank(); // Instance를 만든다
+        Tank tank2 = new Tank(); // Instance를 만든다
+        Tank tank3 = new Tank(); // Instance를 만든다
+        Tank tank4 = new Tank(); // Instance를 만든다
+        Tank tank5 = new Tank(); // Instance를 만든다
     }
 
-    float _yAngle = 0.0f;
 
     void Update()
     {
-        _yAngle += Time.deltaTime * 100.0f;
-        //transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f); 
-
-        //transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100.0f, 0.0f));
-
-        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
         
-
+        
+    }
+    void OnKeyboard()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            
+
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            //transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
         }
-           
+
         if (Input.GetKey(KeyCode.S))
         {
-            
+
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            // transform.Translate(Vector3.back * Time.deltaTime * _speed);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
         }
-            
+
         if (Input.GetKey(KeyCode.A))
         {
-           
+
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            // transform.Translate(Vector3.left * Time.deltaTime * _speed);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
         }
-            
+
         if (Input.GetKey(KeyCode.D))
         {
-            
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            // transform.Translate(Vector3.right * Time.deltaTime * _speed);
-        }
-            
 
-        //transform
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
     }
 }
