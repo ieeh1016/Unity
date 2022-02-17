504aef4ff8166d6578c107687ee35c33ff5716a0
영우 <ieeh0122@naver.com>
2022-02-17 16:43:43 +0900

    Unity

--- Assets/Scripts/Scenes/BaseScene.cs
+++ Assets/Scripts/Scenes/BaseScene.cs
@@ -1,16 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class BaseScene : MonoBehaviour
+public abstract class BaseScene : MonoBehaviour
 {
-    void Start()
+    public Define.Scene SceneType { get; protected set; } = Define.Scene.Unknown;
+
+	void Awake()
+	{
+		Init();
+	}
+
+	protected virtual void Init()
     {
-        
+        Object obj = GameObject.FindObjectOfType(typeof(EventSystem));
+        if (obj == null)
+            Managers.Resource.Instantiate("UI/EventSystem").name = "@EventSystem";
     }
 
-    void Update()
-    {
-        
-    }
+    public abstract void Clear();
 }
